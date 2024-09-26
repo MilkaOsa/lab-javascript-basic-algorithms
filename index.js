9570079521aa5f43d6e2a683b544c5a43663fae5
Estela <estela.valiente@gmail.com>
2024-09-26 20:26:36 +0100

    iteracion 3

--- index.js
+++ index.js
@@ -28,4 +28,29 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
+// 3.1 Print the characters of the driver's name, separated by space, and in capital letters.
+let driverNameInCaps = "";
+for (let i = 0; i < hacker1.length; i++) {
+  driverNameInCaps += hacker1[i].toUpperCase() + " ";
+}
+console.log(driverNameInCaps.trim());  // trim to remove the last extra space
+
+// 3.2 Print all the characters of the navigator's name in reverse order.
+let navigatorNameReversed = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  navigatorNameReversed += hacker2[i];
+}
+console.log(navigatorNameReversed);
+
+// 3.3 Lexicographic order comparison
+if (hacker1 < hacker2) {
+    console.log("The driver's name goes first.");
+  } else if (hacker1 > hacker2) {
+    console.log("Yo, the navigator goes first, definitely.");
+  } else {
+    console.log("What?! You both have the same name?");
+  }
+  
+  
+
 
